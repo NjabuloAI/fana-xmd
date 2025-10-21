27d9dbbb2e9eb31ca384557b241fc2aa15c35f84
Njabulo Jb <fanajbai@gmail.com>
2025-10-21 18:16:24 +0200

    Update owner.js

--- commandes/owner.js
+++ commandes/owner.js
@@ -35,6 +35,46 @@ fana({ nomCom: "owner", categorie: "General", reaction: "❣️" }, async (dest,
         }
       )
   } else {
+       // List of image URLs
+    const njabulox = [
+        "https://files.catbox.moe/iii5jv.jpg",
+        "https://files.catbox.moe/xjeyjh.jpg",
+        "https://files.catbox.moe/mh36c7.jpg",
+        "https://files.catbox.moe/u6v5ir.jpg",
+        "https://files.catbox.moe/bnb3vx.jpg" // New image added
+    ];
+
+    // Select a random image file
+    const randomNjabulourl = njabulox[Math.floor(Math.random() * njabulox.length)];
+    
+
+
+  await zk.sendMessage(chatId, {
+    text,
+    contextInfo: {
+      externalAdReply: {
+          title: `ᥕᥱᥣᥴomᥱ fᥲmιᥣყ `,
+           mediaType: 1,
+          previewType: 0,
+              renderLargerThumbnail: false,
+              thumbnailUrl: randomNjabulourl,
+                     
+                 },
+             },
+          }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "njᥲbᥙᥣo",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=26777821911:+26777821911\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
+    
     const vcard =
         'BEGIN:VCARD\n' + // metadata of the contact card
         'VERSION:3.0\n' +
