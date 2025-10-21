b866e9022c5e02bbb1541013b9ed3d806a38c36e
Njabulo Jb <fanajbai@gmail.com>
2025-10-21 22:49:59 +0200

    Update owner.js

--- commandes/owner.js
+++ commandes/owner.js
@@ -7,17 +7,7 @@ fana({ nomCom: "owner", categorie: "General", reaction: "❣️" }, async (dest,
     
   const thsudo = await isSudoTableNotEmpty()
        // List of image URLs
-    const njabulox = [
-        "https://files.catbox.moe/iii5jv.jpg",
-        "https://files.catbox.moe/xjeyjh.jpg",
-        "https://files.catbox.moe/mh36c7.jpg",
-        "https://files.catbox.moe/u6v5ir.jpg",
-        "https://files.catbox.moe/bnb3vx.jpg" // New image added
-    ];
-
-    // Select a random image file
-    const randomNjabulourl = njabulox[Math.floor(Math.random() * njabulox.length)];
-    
+        
   if (thsudo) {
      let msg = `*My Super-User*\n
      *Owner Number*\n :
@@ -55,33 +45,14 @@ fana({ nomCom: "owner", categorie: "General", reaction: "❣️" }, async (dest,
         'TEL;type=CELL;type=VOICE;waid=' + conf.NUMERO_OWNER + ':+' + conf.NUMERO_OWNER + '\n' + // WhatsApp ID + phone number
         'END:VCARD';
 
-      const njabuloai = `
-     *🎆 Welcome to *Njabulo Jb*
-     *👤Owner number* 77821911
-     
-🌐owner bot *➥ sir Njabulo Jbメ*`,
-
-    await zk.sendMessage(dest, {
-  image: { url: randomNjabulourl },
-    caption: njabuloai,
-    contextInfo: {
-      externalAdReply: {
-          title: "Message owner dev Tech",
-           mediaType: 1,
-          previewType: 0,
-              renderLargerThumbnail: false,
-              thumbnailUrl: randomNjabulourl,
-      }
-    }
- }, { quoted: m });   
-
    zk.sendMessage(dest, {
         contacts: {
             displayName: conf.OWNER_NAME,
             contacts: [{ vcard }],
         },
     },{quoted:ms});
-
+        }
+ });
 
  // Send the audio as a voice note after the ping message
       const audioUrl = 'https://files.catbox.moe/4ufunx.mp3';
@@ -90,5 +61,4 @@ fana({ nomCom: "owner", categorie: "General", reaction: "❣️" }, async (dest,
         mimetype: 'audio/mp4',
         ptt: true
       }, { quoted: m });
-        }
- });
+
